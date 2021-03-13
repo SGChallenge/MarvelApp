919369d7060a414c681ed07e32b9838258fcf9b2
Sguarino93 <santiagodguarino@gmail.com>
2021-03-12 23:27:10 -0300

    test fix

--- app/src/test/java/com/example/marvelchallenge/ServiceTests.kt
+++ app/src/test/java/com/example/marvelchallenge/ServiceTests.kt
@@ -2,6 +2,7 @@ package com.example.marvelchallenge
 
 import com.example.marvelchallenge.data.di.ApiModule
 import com.example.marvelchallenge.data.networking.MarvelService
+import com.example.marvelchallenge.utils.toMD5
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.Job
@@ -17,18 +18,20 @@ class ServiceTests {
     private val service: MarvelService = ApiModule.provideMarvelService()
     private val testDispatcher = TestCoroutineDispatcher()
     private val managedCoroutineScope: ManagedCoroutineScope = TestScope(testDispatcher)
+    private val ts = System.currentTimeMillis()
+    private val hash = (ts.toString() + BuildConfig.PRIVATE_API_KEY + BuildConfig.API_KEY).toMD5()
 
     @Test
     fun getCharacters_test() {
         managedCoroutineScope.launch {
-            assertTrue(service.getPagedCharacters().data.results.isNotEmpty())
+            assertTrue(service.getPagedCharacters(ts, 0, hash).data.results.isNotEmpty())
         }
     }
 
     @Test
     fun getEvents_test() {
         managedCoroutineScope.launch {
-            assertTrue(service.getEvents().data.results.isNotEmpty())
+            assertTrue(service.getEvents(ts, hash).data.results.isNotEmpty())
         }
     }
 }
